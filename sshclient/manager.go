aac6d3a7eb225ae7165ab9b84748b0058eff88a2
Anatoly Usov <usov-ap@outlook.com>
2026-06-22 15:12:16 +0500

    fix(ssh): add support for legacy ciphers, key exchanges and MACs for older equipment like Cisco 1921

--- sshclient/manager.go
+++ sshclient/manager.go
@@ -57,6 +57,36 @@ func Connect(host database.Host) error {
 		User:            host.User,
 		Auth:            authMethods,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // Insecure for demo, should verify in production
+		Config: ssh.Config{
+			Ciphers: []string{
+				"aes128-gcm@openssh.com",
+				"aes256-gcm@openssh.com",
+				"chacha20-poly1305@openssh.com",
+				"aes128-ctr",
+				"aes192-ctr",
+				"aes256-ctr",
+				"aes128-cbc",
+				"3des-cbc",
+				"aes192-cbc",
+				"aes256-cbc",
+			},
+			KeyExchanges: []string{
+				"curve25519-sha256",
+				"curve25519-sha256@libssh.org",
+				"ecdh-sha2-nistp256",
+				"ecdh-sha2-nistp384",
+				"ecdh-sha2-nistp521",
+				"diffie-hellman-group14-sha256",
+				"diffie-hellman-group14-sha1",
+				"diffie-hellman-group1-sha1",
+			},
+			MACs: []string{
+				"hmac-sha2-256-etm@openssh.com",
+				"hmac-sha2-256",
+				"hmac-sha1",
+				"hmac-sha1-96",
+			},
+		},
 	}
 	
 	addr := fmt.Sprintf("%s:%d", host.Address, host.Port)
